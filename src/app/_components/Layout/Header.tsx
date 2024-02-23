d155182ee33d7d3f835644f92bd29d171d9bc6d6
SKitttz <skittz.dev@gmail.com>
2024-02-23 09:26:16 -0300

    refact: animation header 1/3

--- src/app/_components/Layout/Header.tsx
+++ src/app/_components/Layout/Header.tsx
@@ -1,6 +1,10 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '../../../../public/logo.png';
+import { useEffect, useState } from 'react';
+import useDebounce from '../../hooks/useDebounce';
+import { usePathname } from 'next/navigation';
 
 interface navItems {
   nameItem: string;
@@ -16,10 +20,36 @@ export default function Header() {
     { nameItem: 'Contact', slug: '/contact' },
   ];
 
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);
+      };
+      window.addEventListener('scroll', handleScroll);
+
+      return () => window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+  const activeLinkPath = usePathname();
   return (
     <>
-      <header className="w-full bg-gray-200 bg-opacity-80 border-b border-b-slate-600 border-opacity-10 px-4 pt-2 mx-auto fixed z-50">
-        <nav className="max-w-5xl mx-auto flex justify-between items-center gap-x-6 font-besley">
+      <header
+        className={`transition-all w-full mx-auto ${
+          scrollPosition !== 0
+            ? 'fixed pt-2 px-4 '
+            : 'fixed bg-slate-100 shadow-sm'
+        } z-50`}
+      >
+        <nav
+          className={`max-w-5xl ${
+            scrollPosition !== 0
+              ? 'border border-amber-950 border-opacity-10 rounded-md bg-slate-100 bg-opacity-90 shadow-sm'
+              : ''
+          } px-4 mx-auto flex justify-between items-center gap-x-6 font-roboto `}
+        >
           <Link className="flex items-center" href={'/'}>
             <h1 className="text-2xl font-besley font-black text-amber-800">
               Next [
@@ -29,21 +59,28 @@ export default function Header() {
           <ul className="flex gap-x-6">
             {navMenuItems.map((item: navItems, index: number) => (
               <li key={index}>
-                <Link className="hover:text-slate-500" href={item.slug}>
+                <Link
+                  className={`transition-colors hover:text-slate-500 ${
+                    item.slug === activeLinkPath && item.slug !== '/'
+                      ? 'font-bold text-amber-700'
+                      : ''
+                  }`}
+                  href={item.slug}
+                >
                   {item.nameItem}
                 </Link>
               </li>
             ))}
           </ul>
-          <div className="flex gap-x-3 items-center">
+          <div className="flex gap-x-5 items-center text-sm">
             <Link className="hover:text-slate-500" href={'/login'}>
               Login
             </Link>
             <Link
               href={'/register'}
-              className="font-bold px-2 py-1 rounded-md bg-amber-700 hover:bg-amber-800 text-slate-100"
+              className="transition-colors font-semibold px-2 py-1 rounded-md bg-amber-600 hover:bg-amber-700 text-gray-200"
             >
-              Get started{' '}
+              Join US{' '}
             </Link>
           </div>
         </nav>
