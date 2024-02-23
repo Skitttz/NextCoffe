b6426a81954968ba237298bc264f068665a770a1
SKitttz <skittz.dev@gmail.com>
2024-02-23 09:27:48 -0300

    style: better footer

--- src/app/_components/Layout/Footer.tsx
+++ src/app/_components/Layout/Footer.tsx
@@ -9,17 +9,18 @@ export default function Footer() {
     { nameItem: 'Imprint', slug: '/imprint' },
   ];
   return (
-    <footer className="w-full bg-gray-200 bg-opacity-80 border-t border-t-slate-600 border-opacity-10 px-4 pt-2 mx-auto mt-24">
-      <nav className="max-w-2xl mx-auto flex justify-center items-center gap-x-4 text-sm">
+    <footer className="w-full font-roboto bg-opacity-80 border-t border-t-slate-600 border-opacity-5 px-4 py-1 mx-auto mt-24">
+      <nav className="max-w-2xl mx-auto flex justify-center items-center gap-x-4 text-sm text-slate-800 ">
         <Link href={'/'}>
           {' '}
           <Image src={Logo} width={50} height={50} alt={'logo header'} />
         </Link>
-        <div className="flex flex-col items-center text-center">
-          <p>
+        <div className="flex items-center text-center">
+          <p> ©2024, Next Coffe |</p>
+          <p className="font-light text-xs ml-1">
             Produced by
             <a
-              className="text-slate-600"
+              className="text-slate-950 font-medium"
               href="https://github.com/Skitttz"
               target="_blank"
             >
@@ -27,7 +28,6 @@ export default function Footer() {
               @Skittz
             </a>
           </p>
-          <p>©Next Coffe - All rights reserved</p>
         </div>
       </nav>
     </footer>
