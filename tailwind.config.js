69b694527beff7765963206eb93413e6a6f659b7
SKitttz <skittz.dev@gmail.com>
2024-03-06 14:32:29 -0300

    chore:  reconfigure css and tailwind to shadcn UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,76 @@
-/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
         besley: ['var(--font-besley)'],
         roboto: ['var(--font-roboto)'],
         inter: ['var(--font-inter)'],
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'coffe-primary': {
+          50: '#f7f5ef',
+          100: '#ece5d5',
+          200: '#dbcbad',
+          300: '#c6ab7e',
+          400: '#b58f5a',
+          500: '#a67c4c',
+          600: '#865e3c',
+          700: '#734d35',
+          800: '#614132',
+          900: '#54392f',
+          950: '#301d18',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
       },
       keyframes: {
         animeDown: {
@@ -36,12 +91,22 @@ module.exports = {
             opacity: 1,
           },
         },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
       },
       animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
         animeDown: 'animeDown 1s ease-in-out forwards',
         animeFade: 'animeFade 1s ease-in forwards',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
