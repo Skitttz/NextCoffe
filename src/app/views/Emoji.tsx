026baf586de366bdbb98509b444fc4871c6737da
SKitttz <skittz.dev@gmail.com>
2024-03-25 10:48:20 -0300

    feat: Emoji view

---
+++ src/app/views/Emoji.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type EmojiProps = {
+  symbol: string;
+  label?: string;
+};
+
+const Emoji: React.FC<EmojiProps> = ({ symbol, label }) => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={label ? label : ''}
+    aria-hidden={label ? 'false' : 'true'}
+  >
+    {symbol}
+  </span>
+);
+
+export default Emoji;
