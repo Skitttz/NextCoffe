ab8abda6293a6dc32a77d778b1c2623821278961
SKitttz <skittz.dev@gmail.com>
2024-03-28 17:38:38 -0300

    feat: page events

---
+++ src/app/events/page.tsx
@@ -0,0 +1,27 @@
+export default function Page() {
+  const infoText = (
+    <div>
+      <p>
+        Indulge in the simple joys of life with Coffee with Case, an event where
+        people come together to savor delicious ☕️ coffee, appreciate 🎨 art,
+        and engage in meaningful conversations about lifes intricacies.
+      </p>
+      <p>
+        Art Appreciation: Immerse yourself in a showcase of captivating artwork,
+        ranging from paintings and sculptures to photography and mixed media
+        creations. Let the creativity inspire you and spark thought-provoking
+        discussions. Coffee Tasting: Treat your taste buds to an array of
+        gourmet coffees sourced from around the world. Learn about different
+        brewing techniques, flavor profiles, and the art of coffee appreciation
+        from our knowledgeable hosts. Conversations and Connections: Engage in
+        meaningful conversations with other attendees, sharing stories,
+        insights, and perspectives on a myriad of topics. Whether It s
+        discussing the latest art trends or pondering life isphilosophical
+        questions, you will find stimulating dialogue and camaraderie at every
+        turn.
+      </p>
+    </div>
+  );
+
+  return <main></main>;
+}
