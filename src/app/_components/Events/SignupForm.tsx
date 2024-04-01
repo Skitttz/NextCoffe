1b16fe52e07bb781f7403300f8974a24db8a1b70
Skittz <skittz.dev@gmail.com>
2024-04-01 00:16:46 -0300

    feat: signUpForm component 1/3

--- src/app/_components/Events/SignupForm.tsx
+++ src/app/_components/Events/SignupForm.tsx
@@ -1,13 +1,28 @@
-const signupForm = ({ title, infoText }) => {
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+
+const SignupForm = ({ title, infoText }) => {
+  const [formData, setFormData] = useState({});
   return (
-    <section className="grid grid-cols-[1fr,300px]">
-      <div>
-        <h3>{title}</h3>
-        {infoText}
+    <section className="grid grid-cols-[1fr,1fr] gap-x-8 items-center">
+      <div className="mt-8">
+        <h3 className="text-4xl font-bold font-besley text-amber-950 mb-4">
+          {title}
+        </h3>
+        <p className="font-roboto max-w-xl">{infoText}</p>
       </div>
-      <form action=""></form>
+      <div>
+        <p className="text-center bg-slate-100 rounded-sm border border-amber-600 border-opacity-20 p-2">
+          To participate in the event you need to be a NextCoffe user, log in to
+          your account{' '}
+          <Link href={'/login'} className="font-bold text-amber-600">
+            clicando aqui.
+          </Link>
+        </p>
+      </div>
     </section>
   );
 };
 
-export default signupForm;
+export default SignupForm;
